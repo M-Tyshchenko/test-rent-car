d4880bb554cf0501625cb10458e6f790e7f3ae44
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-07 16:58:13 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,24 @@
-import Catalog from "pages/CatalogPage/Catalog";
-import Favorites from "pages/FavoritePage/Favorites";
-import Home from "pages/HomePage/Home";
-import { Route, Routes } from "react-router-dom";
+// import Catalog from 'pages/CatalogPage/Catalog';
+// import Favorites from 'pages/FavoritePage/Favorites';
+// import Home from 'pages/HomePage/Home';
+import { Route, Routes } from 'react-router-dom';
+import Layout from './Layout/Layout';
+import { Home } from 'pages/HomePage/Home';
+import { Catalog } from 'pages/CatalogPage/Catalog';
+import { Favorites } from 'pages/FavoritePage/Favorites';
+
 
 export const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/catalog" element={<Catalog />} />
-      <Route path="/favorites" element={<Favorites />} />
-      <Route path="*" element={<Home />} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/catalog" element={<Catalog />} />
+        {/* <Route path="/register" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} /> */}
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="*" element={<Home />} />
+      </Route>
     </Routes>
   );
 };
