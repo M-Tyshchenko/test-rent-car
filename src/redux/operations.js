dc8375915a337a92ab3aaab6fe8c4c61e414b00d
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-07 16:58:56 +0200

    Create operations.js

---
+++ src/redux/operations.js
@@ -0,0 +1,52 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+axios.defaults.baseURL = '<https://6570852909586eff66418c8d.mockapi.io>';
+
+export const fetchCars = createAsyncThunk(
+  'cars/fetchAll',
+  async (_, thunkApi) => {
+    try {
+      const response = await axios.get('/advert');
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const fetchFavoriteCars = createAsyncThunk(
+  'favorites/fetchAllFavorite',
+  async (_, thunkApi) => {
+    try {
+      const response = await axios.get('/favorites');
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addFavorite = createAsyncThunk(
+  'favorites/addFavorite',
+  async (favoriteCar, thunkApi) => {
+    try {
+      const response = await axios.post('/favorites', favoriteCar);
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteFavorite = createAsyncThunk(
+  'favorites/deleteFavorite',
+  async (carId, thunkApi) => {
+    try {
+      const response = await axios.delete(`/favorites/${carId}`);
+      return response.data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  }
+);
