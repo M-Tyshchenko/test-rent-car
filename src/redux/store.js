73b735c2f04c76aab91f47dd9a05a3e956a4d4ff
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-07 16:59:01 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { carsReducer } from "./carsSlice";
+import { filterReducer } from "./filterSlice";
+
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedCarsReducer = persistReducer(persistConfig, carsReducer);
+
+export const store = configureStore({
+    reducer: {
+        cars: persistedCarsReducer,
+        filter: filterReducer,
+    },
+    middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
