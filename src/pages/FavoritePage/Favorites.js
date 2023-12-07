ee5072ea2294a45724c9075504389d1adebf6852
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-07 16:58:31 +0200

    Update Favorites.js

--- src/pages/FavoritePage/Favorites.js
+++ src/pages/FavoritePage/Favorites.js
@@ -1,7 +1,7 @@
-const Favorites = () => {
+export const Favorites = () => {
   return (
     <h1>Your favourite cars for rent</h1>
   );
 };
 
-export default Favorites;
+// export default Favorites;
