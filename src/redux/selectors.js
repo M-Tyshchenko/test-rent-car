06a2ca6b906120995a6859318878c95f8800f8a3
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 01:59:04 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,16 +1,37 @@
-import { createSelector } from "@reduxjs/toolkit";
+import { createSelector } from '@reduxjs/toolkit';
 
 export const selectCars = state => state.cars.cars;
 export const selectFavoriteCars = state => state.cars.favorites;
 export const selectIsLoading = state => state.cars.isLoading;
 export const selectError = state => state.cars.error;
-export const selectFilterByBrand = state => state.filter.filter;
+export const selectFilterByBrand = state => state.filter.filterByBrand;
+export const selectFilterByPrice = state => state.filter.filterByPrice;
+
+export const selectVisibleCarsByBrand = createSelector(
+  [selectCars, selectFilterByBrand],
+  (cars, filterByBrand) =>
+    cars.filter(car =>
+      car.make.toLowerCase().includes(filterByBrand.toLowerCase())
+    )
+);
+
+// export const selectVisibleCarsByPrice = createSelector(
+//   [selectCars, selectFilterByPrice],
+//   (cars, filterByPrice) =>
+//     cars.filter(
+//       car =>
+//         Number(car.rentalPrice.slice(1, car.rentalPrice.length)) <=
+//         filterByPrice
+//     )
+// );
 
 export const selectVisibleCars = createSelector(
-  [selectCars, selectFilterByBrand],
-  (cars, filter) =>
-    cars.filter(car =>
-      car.make.includes(filter)
+  [selectVisibleCarsByBrand, selectFilterByPrice],
+  (filteredByBrand, filterByPrice) =>
+    filteredByBrand.filter(
+      car =>
+        Number(car.rentalPrice.slice(1, car.rentalPrice.length)) <=
+        filterByPrice
     )
 );
 
@@ -18,6 +39,6 @@ export const selectVisibleFavorites = createSelector(
   [selectFavoriteCars, selectFilterByBrand],
   (favorites, filter) =>
     favorites.filter(car =>
-      car.make.includes(filter)
+      car.make.toLowerCase().includes(filter.toLowerCase())
     )
 );
