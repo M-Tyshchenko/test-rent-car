1749fd739748fdf0a90c4da18c6c731af158900a
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 01:58:54 +0200

    Update Favorites.js

--- src/pages/FavoritePage/Favorites.js
+++ src/pages/FavoritePage/Favorites.js
@@ -1,6 +1,41 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { ThreeDots } from 'react-loader-spinner';
+import { fetchFavoriteCars } from 'redux/operations';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { SearchBar } from 'components/SearchBar/SearchBar';
+import { CarGallery } from 'components/CarGallery/CarGallery';
+
 export const Favorites = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchFavoriteCars());
+  }, [dispatch]);
   return (
-    <h1>Your favourite cars for rent</h1>
+    // <h1>Your favourite cars for rent</h1>
+    <>
+      <SearchBar />
+      {isLoading && !error && (
+        <ThreeDots
+          height="80"
+          width="80"
+          radius="9"
+          color="#3470FF"
+          ariaLabel="three-dots-loading"
+          wrapperStyle={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+          wrapperClassName=""
+          visible={true}
+        />
+      )}
+      <CarGallery />
+    </>
   );
 };
 
