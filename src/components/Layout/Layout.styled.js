60378ce90c54fbce8433b1251b2e35f9fab3c5cd
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-08 17:32:08 +0200

    Update Layout.styled.js

---
+++ src/components/Layout/Layout.styled.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const LayoutContainer = styled.div`
+  margin: 0 auto;
+  padding: 0 128px;
+`;
+
+export const PageContainer = styled.div`
+  padding-top: 25px;
+  padding-bottom: 25px;
+  /* display: flex; */
+  /* justify-content: center; */
+`;
