3ba795b374395b78c9bc7db026dab886663750fc
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 01:58:59 +0200

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -2,16 +2,18 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 axios.defaults.baseURL = 'https://6570852909586eff66418c8d.mockapi.io';
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
+axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
 
 export const fetchCars = createAsyncThunk(
   'cars/fetchAll',
   async (_, thunkApi) => {
     try {
       const response = await axios.get('/advert');
-      
+
       return response.data;
     } catch (error) {
-      console.error(error);
+      // console.error(error);
       return thunkApi.rejectWithValue(error.message);
     }
   }
