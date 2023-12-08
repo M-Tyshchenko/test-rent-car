a30cebd0b640ad8e924fa1376802b23e8a852f56
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-08 17:31:42 +0200

    Update AppBar.styled.js

--- src/components/AppBar/AppBar.styled.js
+++ src/components/AppBar/AppBar.styled.js
@@ -1,2 +1,24 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
+
+export const Header = styled.header`
+  margin: 0 auto;
+  padding: 25px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const LinkContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 40px;
+`;
+
+export const StyledLink = styled(NavLink)`
+  font-weight: 600;
+  font-size: 18px;
+
+  &.active {
+    color: #3470ff;
+  }
+`;
