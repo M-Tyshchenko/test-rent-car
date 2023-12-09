a77f36a25509e90c10b04abdf766a298ca8bc320
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 23:34:16 +0200

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -7,27 +7,8 @@ export const AppBar = () => {
         <LinkContainer>
           <StyledLink to="/">Home</StyledLink>
           <StyledLink to="/catalog">Catalog</StyledLink>
-          {/* if logged in show favorites page */}
           <StyledLink to="/favorites">Favourite</StyledLink>
         </LinkContainer>
-
-        {/* <div> */}
-        {/* Logged in user menu */}
-        {/* <div> */}
-        {/* <p>Welcome, user</p> */}
-        {/* <button type="submit">Logout</button> */}
-        {/* </div> */}
-        {/* OR */}
-        {/* Register menu */}
-        {/* <ul>
-          <li>
-            <NavLink to="/register">Sign up</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </ul> */}
-        {/* </div> */}
       </Header>
       <hr />
     </>
