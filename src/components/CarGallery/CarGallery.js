b4cbc6e8661aa241f199f1d2b8fb8bc53c8cde57
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 23:34:19 +0200

    Update CarGallery.js

--- src/components/CarGallery/CarGallery.js
+++ src/components/CarGallery/CarGallery.js
@@ -1,22 +1,13 @@
 import { CarGalleryItem } from 'components/CarGalleryItem/CarGalleryItem';
 import { useSelector } from 'react-redux';
-import {
-  selectFilterByPrice,
-  selectVisibleCars,
-  selectVisibleCarsByBrand,
-} from 'redux/selectors';
+import { selectVisibleCarsByBrand } from 'redux/selectors';
 import { CarGalleryContainer, CarList, LoadMoreBtn } from './CarGallery.styled';
 
 export const CarGallery = () => {
-  const filterByPrice = useSelector(selectFilterByPrice);
-
   const carsByBrand = useSelector(selectVisibleCarsByBrand);
-  const carsByBrandAndPrice = useSelector(selectVisibleCars);
 
   let cars = carsByBrand;
-  if (filterByPrice !== 0) {
-    cars = carsByBrandAndPrice;
-  }
+
   // const dispatch = useDispatch();
 
   return (
