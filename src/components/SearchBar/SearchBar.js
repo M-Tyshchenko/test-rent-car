6d299b7ddb0b380d4f3b73375c1ae966f51a2d6c
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-09 23:34:46 +0200

    Update SearchBar.js

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,79 +1,94 @@
-// import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Select from 'react-select';
-import { changeFilterByBrand, changeFilterByPrice } from 'redux/filterSlice';
+import { changeFilterBrand, changeFilterPrice } from 'redux/filterSlice';
 import { selectFilterByBrand, selectFilterByPrice } from 'redux/selectors';
+
 import {
   Form,
   FormLabel,
   InputBox,
   Input,
   SearchBtn,
+  SelectBrand,
+  SelectPrice,
 } from './SearchBar.styled';
 
-const brandOptions = [
-  { value: 'buick', label: 'Buick' },
-  { value: 'volvo', label: 'Volvo' },
-  { value: 'hummer', label: 'HUMMER' },
-  { value: 'subaru', label: 'Subaru' },
-  { value: 'mitsubishi', label: 'Mitsubishi' },
-  { value: 'nissan', label: 'Nissan' },
-  { value: 'lincoln', label: 'Lincoln' },
-  { value: 'gmc', label: 'GMC' },
-  { value: 'hyundai', label: 'Hyundai' },
-  { value: 'mini', label: 'MINI' },
-  { value: 'bentley', label: 'Bentley' },
-  { value: 'mercedes', label: 'Mercedes-Benz' },
-  { value: 'aston', label: 'Aston Martin' },
-  { value: 'pontiac', label: 'Pontiac' },
-  { value: 'lamborghini', label: 'Lamborghini' },
-  { value: 'audi', label: 'Audi' },
-  { value: 'bmw', label: 'BMW' },
-  { value: 'chevrolet', label: 'Chevrolet' },
-  { value: 'chrysler', label: 'Chrysler' },
-  { value: 'kia', label: 'Kia' },
-  { value: 'land', label: 'Land' },
-];
-let priceOption = [];
-for (let i = 10; i <= 500; i += 10) {
-  priceOption.push({ value: i, label: i });
-}
-
 export const SearchBar = () => {
-  // const [selectedOption, setSelectedOption] = useState('');
   const dispatch = useDispatch();
   const filterByBrand = useSelector(selectFilterByBrand);
   const filterByPrice = useSelector(selectFilterByPrice);
 
+  const brandOptions = [
+    { value: 'buick', label: 'Buick' },
+    { value: 'volvo', label: 'Volvo' },
+    { value: 'hummer', label: 'HUMMER' },
+    { value: 'subaru', label: 'Subaru' },
+    { value: 'mitsubishi', label: 'Mitsubishi' },
+    { value: 'nissan', label: 'Nissan' },
+    { value: 'lincoln', label: 'Lincoln' },
+    { value: 'gmc', label: 'GMC' },
+    { value: 'hyundai', label: 'Hyundai' },
+    { value: 'mini', label: 'MINI' },
+    { value: 'bentley', label: 'Bentley' },
+    { value: 'mercedes', label: 'Mercedes-Benz' },
+    { value: 'aston', label: 'Aston Martin' },
+    { value: 'pontiac', label: 'Pontiac' },
+    { value: 'lamborghini', label: 'Lamborghini' },
+    { value: 'audi', label: 'Audi' },
+    { value: 'bmw', label: 'BMW' },
+    { value: 'chevrolet', label: 'Chevrolet' },
+    { value: 'chrysler', label: 'Chrysler' },
+    { value: 'kia', label: 'Kia' },
+    { value: 'land', label: 'Land' },
+  ];
+  let priceOptions = [{ value: '', label: 'To $' }];
+  for (let i = 10; i <= 500; i += 10) {
+    priceOptions.push({ value: i, label: i });
+  }
+
   return (
     <>
-      <Form>
+      <Form
+        onSubmit={event => {
+          event.preventDefault();
+          dispatch(changeFilterBrand(event.target[0].value));
+          dispatch(changeFilterPrice(event.target[1].value));
+        }}
+      >
         <FormLabel>
           Car brand
-          <Select
-            placeholder="Enter the text"
+          <SelectBrand
             defaultValue={filterByBrand}
-            onChange={event => {
-              console.log(event);
-              dispatch(changeFilterByBrand(event.value));
-            }}
-            options={brandOptions}
-          />
+            // onChange={event => {
+            //   dispatch(changeFilterBrand(event.target.value));
+            // }}
+          >
+            <option value="">Enter the text</option>
+            {brandOptions.map(option => (
+              <option
+                key={option.value}
+                value={option.value}
+                label={option.label}
+              />
+            ))}
+          </SelectBrand>
         </FormLabel>
 
         <FormLabel>
           Price/1hour
-          <Select
-            placeholder="To $"
-            autosuggest="false"
+          <SelectPrice
             defaultValue={filterByPrice}
-            onChange={event => {
-              console.log(event.value);
-              dispatch(changeFilterByPrice(event.value));
-            }}
-            options={priceOption}
-            styles={{ color: 'red' }}
-          />
+            // onChange={event => {
+            //   dispatch(changeFilterPrice(event.target.value));
+            // }}
+          >
+            {priceOptions.map(option => (
+              <option
+                key={option.value}
+                value={option.value}
+                label={option.label}
+              />
+            ))}
+          </SelectPrice>
         </FormLabel>
         <FormLabel>
           Car mileage / km
@@ -102,25 +117,3 @@ export const SearchBar = () => {
     </>
   );
 };
-
-//  <option value="buick" label="Buick" />
-// <option value="volvo" label="Volvo" />
-// <option value="hummer" label="HUMMER" />
-// <option value="subaru" label="Subaru" />
-// <option value="mitsubishi" label="Mitsubishi" />
-// <option value="nissan" label="Nissan" />
-// <option value="lincoln" label="Lincoln" />
-// <option value="gmc" label="GMC" />
-// <option value="hyundai" label="Hyundai" />
-// <option value="mini" label="MINI" />
-// <option value="bentley" label="Bentley" />
-// <option value="mercedes" label="Mercedes-Benz" />
-// <option value="aston" label="Aston Martin" />
-// <option value="pontiac" label="Pontiac" />
-// <option value="lamborghini" label="Lamborghini" />
-// <option value="audi" label="Audi" />
-// <option value="bmw" label="BMW" />
-// <option value="chevrolet" label="Chevrolet" />
-// <option value="chrysler" label="Chrysler" />
-// <option value="kia" label="Kia" />
-// <option value="land" label="Land" />
