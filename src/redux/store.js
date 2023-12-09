8ce32b27e7b24101c718d2f2dcc6a442ba8fe82d
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-10 00:15:40 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,18 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { persistStore, persistReducer, FLUSH, PAUSE, PERSIST, PURGE, REGISTER, REHYDRATE } from 'redux-persist';
+import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
 import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-import { carsReducer } from "./carsSlice";
-import { filterReducer } from "./filterSlice";
-
+import { carsReducer } from './carsSlice';
+import { filterReducer } from './filterSlice';
+import { modalReducer } from './modalSlice';
 
 const persistConfig = {
   key: 'root',
@@ -13,11 +22,12 @@ const persistConfig = {
 const persistedCarsReducer = persistReducer(persistConfig, carsReducer);
 
 export const store = configureStore({
-    reducer: {
-        cars: persistedCarsReducer,
-        filter: filterReducer,
-    },
-    middleware: getDefaultMiddleware =>
+  reducer: {
+    cars: persistedCarsReducer,
+    filter: filterReducer,
+    modal: modalReducer,
+  },
+  middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
       serializableCheck: {
         ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
