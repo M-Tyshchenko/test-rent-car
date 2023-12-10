09559744975158dc2d0de9f21b3e03edfb756f01
M-Tyshchenko <m.tish2406@gmail.com>
2023-12-10 15:32:35 +0200

    Update CarGalleryItem.styled.js

--- src/components/CarGalleryItem/CarGalleryItem.styled.js
+++ src/components/CarGalleryItem/CarGalleryItem.styled.js
@@ -1,12 +1,14 @@
 import styled from 'styled-components';
 import { FaRegHeart } from 'react-icons/fa';
 import { FaHeart } from 'react-icons/fa';
+import Modal from 'react-modal';
 
 export const ListItem = styled.li`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 28px;
+  height: 426px;
 `;
 
 export const ImageContainer = styled.div`
@@ -28,15 +30,20 @@ export const Image = styled.img`
   } */
 `;
 
-export const FavoriteBtn = styled(FaHeart)`
+export const FavoriteTrueBtn = styled(FaHeart)`
   position: absolute;
   top: 14px;
   right: 14px;
-  /* color: red; */
-  fill: #3470ff;
-  stroke: #3470ff;
-  /* background-color: aqua; */
-  /* fill: rgba(255, 255, 255, 0.8); */
+   color: #3470ff;
+
+`;
+
+export const FavoriteFalseBtn = styled(FaRegHeart)`
+  position: absolute;
+  top: 14px;
+  right: 14px;
+
+  color: rgba(255, 255, 255, 0.8);
 `;
 
 export const CardTitle = styled.div`
@@ -45,7 +52,7 @@ export const CardTitle = styled.div`
   margin-bottom: 8px;
   font-weight: 500;
   font-size: 16px;
-  line-height: 24px;
+  line-height: 1.5;
   color: #121417;
 `;
 
@@ -54,10 +61,9 @@ export const ColorAccent = styled.span`
 `;
 
 export const CardDescription = styled.p`
-  margin-bottom: 28px;
   font-weight: 400;
   font-size: 12px;
-  line-height: 18px;
+  line-height: 1.5;
   color: rgba(18, 20, 23, 0.5);
 `;
 
@@ -75,7 +81,100 @@ export const LearnMoreBtn = styled.button`
   }
 `;
 
+export const PopUp = styled(Modal)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1200;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.2);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ModalContainer = styled.div`
+  position: relative;
+  padding: 40px;
+  /* display: flex; */
+  width: 541px;
+  border-radius: 24px;
+  background-color: #fff;
+`;
+
+export const ModalCloseButton = styled.button`
+  position: absolute;
+  top: 16px;
+  right: 16px;
+  width: 24px;
+  height: 24px;
+`;
+
 export const ModalImage = styled.img`
-  max-width: calc(100vw - 48px);
-  max-height: calc(100vh - 24px);
+  width: 461px;
+  border-radius: 14px;
+  margin-bottom: 14px;
+
+  /* max-width: calc(100vw - 48px);
+  max-height: calc(100vh - 24px); */
+`;
+export const CardContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export const DescrContainer = styled.div`
+  margin-bottom: 14px;
+`;
+
+export const CarDescr = styled.p`
+  font-weight: 400;
+  line-height: 1.43;
+  font-size: 14px;
+`;
+
+export const CarCharacteristicsTitle = styled.h2`
+  margin-bottom: 8px;
+  font-weight: 400;
+  line-height: 1.43;
+  font-size: 14px;
+`;
+
+export const ConditionsList = styled.ul`
+  display: flex;
+  gap: 8px;
+  flex-wrap: wrap;
+`;
+
+export const ConditionsListItem = styled.li`
+  padding: 7px 14px;
+  border-radius: 35px;
+  background-color: #f9f9f9;
+  color: #363535;
+  font-weight: 400;
+  font-size: 12px;
+  line-height: 1.5;
+  letter-spacing: -0.02em;
+`;
+
+export const RentalCarBtn = styled.a`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 168px;
+  height: 44px;
+
+  color: #fff;
+  font-size: 14px;
+  background-color: #3470ff;
+  border-radius: 12px;
+  cursor: pointer;
+
+  &:hover,
+  &:focus {
+    background-color: #0b44cd;
+  }
 `;
