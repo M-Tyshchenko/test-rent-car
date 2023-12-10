a804ddb2baf5e526dd29570ad4b8872c5ca84078
M-Tyshchenko <122963239+M-Tyshchenko@users.noreply.github.com>
2023-12-10 21:29:49 +0000

    Deploying to gh-pages from @ M-Tyshchenko/test-rent-car@aca540bc313e79c08babcccef1d6c58299c7db24 🚀

---
+++ static/js/375.d04b9e86.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_template=self.webpackChunkreact_template||[]).push([[375],{8375:function(e,t,a){a.r(t);var c=a(184);t.default=function(){return(0,c.jsx)("h1",{children:"Welcome to rent car app"})}}}]);
+//# sourceMappingURL=375.d04b9e86.chunk.js.map
