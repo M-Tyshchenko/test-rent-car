782787723fd4f290b5c812d74c520b42f0c80c42
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-11 15:17:19 +0200

    Update CarGallery.js

--- src/components/CarGallery/CarGallery.js
+++ src/components/CarGallery/CarGallery.js
@@ -1,13 +1,14 @@
 import { CarGalleryItem } from 'components/CarGalleryItem/CarGalleryItem';
 import { useSelector } from 'react-redux';
 import { selectVisibleCars, selectVisibleFavorites } from 'redux/selectors';
-import { CarGalleryContainer, CarList } from './CarGallery.styled';
+import { CarList } from './CarGallery.styled';
 import { useLocation } from 'react-router-dom';
 
 export const CarGallery = () => {
   const location = useLocation();
   const catalogCars = useSelector(selectVisibleCars);
   const favoriteCars = useSelector(selectVisibleFavorites);
+
   let cars = catalogCars;
 
   if (location.pathname === '/favorites') {
@@ -15,13 +16,12 @@ export const CarGallery = () => {
   }
 
   return (
-    <CarGalleryContainer>
+    <>
       <CarList>
         {cars.map(car => (
           <CarGalleryItem key={car.id} car={car} />
         ))}
       </CarList>
-      {/* <LoadMoreBtn type="button">Load More</LoadMoreBtn> */}
-    </CarGalleryContainer>
+    </>
   );
 };
