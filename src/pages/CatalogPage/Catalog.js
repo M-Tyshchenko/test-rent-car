39d02645111c95cb98597f091d9a3cbe3d819668
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-11 15:18:00 +0200

    Update Catalog.js

--- src/pages/CatalogPage/Catalog.js
+++ src/pages/CatalogPage/Catalog.js
@@ -1,22 +1,40 @@
 import { CarGallery } from 'components/CarGallery/CarGallery';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchCars } from 'redux/operations';
-import { selectError, selectIsLoading } from 'redux/selectors';
+import {
+  selectError,
+  selectIsLoading,
+  selectTotalCars,
+  // selectVisibleCars,
+} from 'redux/selectors';
 import { SearchBar } from 'components/SearchBar/SearchBar';
 import { ThreeDots } from 'react-loader-spinner';
+import { CatalogContainer, LoadMoreBtn } from './Catalog.styled';
 
 const Catalog = () => {
   const dispatch = useDispatch();
+
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+  const totalCars = useSelector(selectTotalCars);
+
+  const [page, setPage] = useState(1);
+  const perPage = 12;
+  const totalPages = Math.ceil(totalCars / perPage);
+
+  console.log({ totalCars });
+  console.log({ totalPages });
+  console.log({ page });
 
   useEffect(() => {
     dispatch(fetchCars());
   }, [dispatch]);
 
+  const LoadMore = () => setPage(prevState => prevState + 1);
+
   return (
-    <>
+    <CatalogContainer>
       <SearchBar />
       {isLoading && !error && (
         <ThreeDots
@@ -35,7 +53,12 @@ const Catalog = () => {
         />
       )}
       <CarGallery />
-    </>
+      {totalCars !== 0 && totalPages !== page && !isLoading && (
+        <LoadMoreBtn type="button" onClick={LoadMore}>
+          Load More
+        </LoadMoreBtn>
+      )}
+    </CatalogContainer>
   );
 };
 
