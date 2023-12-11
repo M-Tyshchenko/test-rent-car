3227a3def248003d4702e740925719beeff18ddc
Mariia Tyshchenko <m.tish2406@gmail.com>
2023-12-11 15:17:54 +0200

    Update selectors.js

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -2,6 +2,8 @@ import { createSelector } from '@reduxjs/toolkit';
 
 export const selectCars = state => state.cars.cars;
 
+export const selectTotalCars = state => state.cars.cars.length;
+
 export const selectFavoriteCars = state => state.favorites.favorites;
 
 export const selectIsLoading = state => state.cars.isLoading;
